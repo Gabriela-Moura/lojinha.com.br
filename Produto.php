682335015fda9594aac703a4959dbb0b05a1de46
Gabriela-Moura <gabriela.m.souza10@aluno.senai.br>
2025-10-07 15:54:29 -0300

    Co-authored-by: Joao-Guiaro <Joao-Guiaro@users.noreply.github.com> Co-authored-by: Leonardo Rodrigues Guidolim Pinho <leonardo.rodriguesguidolim@gmail.com>

---
+++ Produto.php
@@ -0,0 +1,36 @@
+<?php
+// ┏━━━━•°•°❀°•°•━━━━┓
+// ✧*̥˚    Anna     *̥˚✧
+// ┗━━━━•°•°❀°•°•━━━━┛
+    class Produto{
+        private $id;
+        private $nome;
+        private $preco;
+        public function __construct($id, $nome, $preco){
+            $this->id = $id;
+            $this->nome = $nome;
+            $this->preco = $preco;
+        }
+        //Getters
+        public function getId(){
+            return $this->id;
+        }
+        public function getNome(){
+            return $this->nome;
+        }
+        public function getPreco(){
+            return $this->preco;
+        }
+        //Setters
+        public function setNome($nome){
+            $this->nome = $nome;
+        }
+        public function setPreco($preco){
+            $this->preco = $preco;
+        }
+        //Converte o objeto para array (útil para salvar JSON)
+        public function toArray($data){
+            return new Produto($data['id'], $data['nome'], $data['preco']);
+        }
+    }
+?>
